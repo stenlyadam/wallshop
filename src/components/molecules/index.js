9b7153be9db13900d011193e4ec452f76bd06bff
FlindyR <s21610608@universitasklabat.onmicrosoft.com>
2022-03-08 11:30:27 +0800

    fix signup isssue

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,6 +2,5 @@ import FormInput from './FormInput';
 import Carousel from './Carousel';
 import SearchBar from './SearchBar';
 import RoundButton from './RoundButton';
-import SignupForm from './SignUpForm';
 
-export {FormInput, Carousel, SearchBar, RoundButton, SignupForm};
+export {FormInput, Carousel, SearchBar, RoundButton };
