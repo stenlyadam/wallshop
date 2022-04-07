a1d6ee7c153fa40b4fc982f2ff573638d9719b67
FlindyR <S21610608@universitasklabat.onmicrosoft.com>
2022-04-07 18:51:06 +0800

    fix dummydata untuk tab toko di mallDetail

---
+++ src/pages/Shop/dummyData.js
@@ -0,0 +1,60 @@
+const dummyData = [
+    {
+      id: 1,
+      name: 'Ace Hardware Mantos',
+      description: 'Toko perlengkapan rumah',
+      rating: '4.9',
+      location: 'Mantos, Kota Manado',
+      image: require('../../assets/images/img-ace-hardware.png'),
+    },
+    {
+      id: 2,
+      name: 'Gramedia',
+      description: 'Buku dan perlengkapan belajar',
+      rating: '5.0',
+      location: 'Mantos, Kota Manado',
+      image: require('../../assets/images/img-gramedia.png'),
+    },
+    {
+      id: 3,
+      name: '3Second',
+      description: 'Fashion & Clothing',
+      rating: '4.8',
+      location: 'Mantos, Kota Manado',
+      image: require('../../assets/images/img-3second.png'),
+    },
+    {
+      id: 4,
+      name: 'Sport Station',
+      description: 'Fashion & Sport',
+      rating: '4.9',
+      location: 'MTC, Kota Manado',
+      image: require('../../assets/images/img-sport-station.png'),
+    },
+    {
+      id: 5,
+      name: 'Miens Souvenir Tikala',
+      description: 'Toko oleh - oleh souvenir',
+      rating: '4.7',
+      location: 'Tikala, Kota Manado',
+      image: require('../../assets/images/img-miens-souvenir-tikala.png'),
+    },
+    {
+      id: 6,
+      name: 'Maskut Sport',
+      description: 'Perlengkapan olahraga basket',
+      rating: '4.8',
+      location: 'MTC, Kota Manado',
+      image: require('../../assets/images/img-maskut-sport.png'),
+    },
+    {
+      id: 7,
+      name: 'Maskut Sport',
+      description: 'Perlengkapan olahraga basket',
+      rating: '4.8',
+      location: 'MTC, Kota Manado',
+      image: require('../../assets/images/img-maskut-sport.png'),
+    },
+  ];
+
+  export default dummyData;
